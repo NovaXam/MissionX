727118c56aabc109b4f48a48b8d71049ce95ccb5
Maksym Novakh <novakhm@gmail.com>
2017-10-16 09:26:15 -0400

    updated scroll bar gallery

--- client/src/components/partials/Footer.js
+++ client/src/components/partials/Footer.js
@@ -2,14 +2,15 @@ import React from 'react';
 import Timer from './Timer';
 import './Footer.css';
 import Spacex_logo from '../../assets/Spacex_logo.png';
+import MarsPhoto from '../../assets/big_deal_mars.jpg';
 
 const Footer = (props) => {
   return (
     <div className="mainFooter">
-      <img id="background" src="https://pi.tedcdn.com/r/pf.tedcdn.com/images/playlists/big_deal_mars_2451387841_1200x627.jpg?c=1050%2C550&w=1050" alt="picture" />
+      <img id="background" src={MarsPhoto} alt="picture" />
       <div className="timerBlock">
         <Timer currentTime={props.timeOver} />
-        <a href="http://www.spacex.com/" targer="_blank"> <img id="spaceXlogo" src={ Spacex_logo } alt="imagepicture" /></a>
+        <a href="http://www.spacex.com/" target="_blank"> <img id="spaceXlogo" src={ Spacex_logo } alt="imagepicture" /></a>
       </div>
     </div>
   );
--- client/src/components/partials/Header.js
+++ client/src/components/partials/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Navigation from './Navigation';
+import {Link} from 'react-router-dom';
 import './Header.css';
 import Logo from '../../assets/total-recall.png';
 
 const Header = (props) => {
   return (
     <div className="header">
-      <img className="logo" src={Logo} alt="roverpicture" />
+      <Link to="/mission"><img className="logo" src={Logo} alt="roverpicture" /></Link>
       <div className="nav">
         <Navigation
           handleAlbumeListen={props.handleAlbumeListen}
