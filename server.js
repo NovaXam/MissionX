196878081d50a2c93eb006cc8f00e73e0815d155
Maksym Novakh <novakhm@gmail.com>
2017-11-01 21:11:07 -0400

    update heroku

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static('./client/public'));
+app.use(express.static('./client/build'));
 
 /*exprees middleware to check a valide token*/
 app.use((req, res, next) => {
